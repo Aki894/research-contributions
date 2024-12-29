05c5bb2261f5f222558a1c8d7e3dfffb3455040a
nanao <aki894@qq.com>
2024-12-29 16:20:09 +0800

    Update unetr.py
    Signed-off-by: nanao <aki894@qq.com>

--- UNETR/BTCV/networks/unetr.py
+++ UNETR/BTCV/networks/unetr.py
@@ -34,7 +34,7 @@ class UNETR(nn.Module):
         hidden_size: int = 768,
         mlp_dim: int = 3072,
         num_heads: int = 12,
-        pos_embed: str = "perceptron",
+        # pos_embed: str = "perceptron",
         norm_name: Union[Tuple, str] = "instance",
         conv_block: bool = False,
         res_block: bool = True,
@@ -73,8 +73,8 @@ class UNETR(nn.Module):
         if hidden_size % num_heads != 0:
             raise AssertionError("hidden size should be divisible by num_heads.")
 
-        if pos_embed not in ["conv", "perceptron"]:
-            raise KeyError(f"Position embedding layer of type {pos_embed} is not supported.")
+        # if pos_embed not in ["conv", "perceptron"]:
+        #     raise KeyError(f"Position embedding layer of type {pos_embed} is not supported.")
 
         self.num_layers = 12
         self.patch_size = (16, 16, 16)
@@ -93,7 +93,7 @@ class UNETR(nn.Module):
             mlp_dim=mlp_dim,
             num_layers=self.num_layers,
             num_heads=num_heads,
-            pos_embed=pos_embed,
+            # pos_embed=pos_embed,
             classification=self.classification,
             dropout_rate=dropout_rate,
         )
