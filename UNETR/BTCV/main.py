ef7f6682d504de370a169d38389baddaa14a804d
nanao <aki894@qq.com>
2024-12-29 15:09:46 +0800

    Update main.py
    Signed-off-by: nanao <aki894@qq.com>

--- UNETR/BTCV/main.py
+++ UNETR/BTCV/main.py
@@ -36,14 +36,14 @@ parser.add_argument("--logdir", default="test", type=str, help="directory to sav
 parser.add_argument(
     "--pretrained_dir", default="./pretrained_models/", type=str, help="pretrained checkpoint directory"
 )
-parser.add_argument("--data_dir", default="/dataset/dataset0/", type=str, help="dataset directory")
+parser.add_argument("--data_dir", default="./dataset/dataset0/", type=str, help="dataset directory")
 parser.add_argument("--json_list", default="dataset_0.json", type=str, help="dataset json file")
 parser.add_argument(
     "--pretrained_model_name", default="UNETR_model_best_acc.pth", type=str, help="pretrained model name"
 )
 parser.add_argument("--save_checkpoint", action="store_true", help="save checkpoint during training")
-parser.add_argument("--max_epochs", default=5000, type=int, help="max number of training epochs")
-parser.add_argument("--batch_size", default=1, type=int, help="number of batch size")
+parser.add_argument("--max_epochs", default=1000, type=int, help="max number of training epochs")
+parser.add_argument("--batch_size", default=4, type=int, help="number of batch size")
 parser.add_argument("--sw_batch_size", default=1, type=int, help="number of sliding window batch size")
 parser.add_argument("--optim_lr", default=1e-4, type=float, help="optimization learning rate")
 parser.add_argument("--optim_name", default="adamw", type=str, help="optimization algorithm")
